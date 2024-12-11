2309089b0273703a9d30a6eb897de5e060dad0b6
Pratik814 <bhusalpratik00@gmail.com>
2024-12-11 09:56:03 +0545

    Team selection Quiz to help interns select suitable team

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PersonalityQuiz from './component/PersonalityQuiz';
+
+function App() {
+  return (
+    <PersonalityQuiz />
+  );
+}
+export default App
